b155efd87388e376145540c39297e282e2eba7b4
bocok11 <32522029+bocok11@users.noreply.github.com>
2017-10-05 00:20:47 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,8 +2,8 @@ const LineConnect = require('./connect');
 let LINE = require('./main.js');
 
 const auth = {
-	authToken: ' token here ',
-	certificate: 'cert here',
+	authToken: 'Eloay0L09HL7njIjgbo5./QlJLSxUHRAYEgZeUGWGLq.tu7wI4usbPxXtSpWfPQiIZUZiW63y2By2qKnN+UnFEw=',
+	certificate: '6d248800ebf3292ae47a7e7d659da97b69affc0530d542b73d2299c1046f9aa2',
 }
 // let client =  new LineConnect(auth);
 let client =  new LineConnect();
